b793871a08d40a8af3451195b380586f6bc92657
APZhukov <a.zhukov1995@gmail.com>
2023-03-12 02:28:50 +0600

    completed

---
+++ TwoDecksWPF/MainWindow.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Contexts;
+
+
+namespace TwoDecksWPF
+{
+    class Card
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
+
+        public Card(Values value, Suits suit)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public string Name
+        {
+            get { return $"{Value} of {Suit}"; }
+        }
+    }
+
+    class Deck : ObservableCollection<Card>
+    {
+        private static Random random = new Random();
+
+
+        public Deck()
+        {
+            Reset();
+        }
+
+
+        public void Reset()
+        {
+            Clear();
+            for (int suit = 0; suit <= 3; suit++)
+                for (int value = 1; value <= 13; value++)
+                    Add(new Card((Values)value, (Suits)suit));
+         }
+
+
+        public Card Deal(int index)
+        {
+            Card cardToDeal = base[index];
+            RemoveAt(index);
+            return cardToDeal;
+        }
+
+
+        public void Shuffle()
+        {
+            List<Card> copy = new List<Card>(this);
+            Clear();
+            while (copy.Count > 0)
+            {
+                int index = random.Next(copy.Count);
+                Card card = copy[index];
+                copy.RemoveAt(index);
+                Add(card);
+            }
+        }
+
+
+        public void Sort()
+        {
+            List<Card> sortedCards = new List<Card>(this);
+            sortedCards.Sort(new CardComparerByValue());
+            Clear();
+            foreach (Card card in sortedCards)
+            {
+                Add(card);
+            }
+        }
+    }
+
+    class CardComparerByValue : IComparer<Card>
+    {
+        public int Compare(Card x, Card y)
+        {
+            if (x.Suit < y.Suit)
+                return -1;
+            if (x.Suit > y.Suit)
+                return 1;
+            if (x.Value < y.Value)
+                return -1;
+            if (x.Value > y.Value)
+                return 1;
+            return 0;
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (Resources["rightDeck"] is Deck rightDeck)
+            {
+                rightDeck.Clear();
+            }
+        }
+
+
+        private void MoveCard(bool leftToRight)
+        {
+            if ((Resources["rightDeck"] is Deck rightDeck)
+            && (Resources["leftDeck"] is Deck leftDeck))
+            {
+                if (leftToRight)
+                {
+                    if (leftDeckListBox.SelectedItem is Card card)
+                    {
+                        leftDeck.Remove(card);
+                        rightDeck.Add(card);
+                    }
+                }
+                else
+                {
+                    if (rightDeckListBox.SelectedItem is Card card)
+                    {
+                        rightDeck.Remove(card);
+                        leftDeck.Add(card);
+                    }
+                }
+            }
+        }
+
+        private void shuffleLeftDeck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resources["leftDeck"] is Deck leftDeck)
+            {
+                leftDeck.Shuffle();
+            }
+        }
+
+    private void clearRightDeck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resources["rightDeck"] is Deck rightDeck)
+            {
+                rightDeck.Clear();
+            }
+        }
+
+        private void resetLeftDeck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resources["leftDeck"] is Deck leftDeck)
+            {
+                leftDeck.Reset();
+            }
+        }
+
+        private void sortRightDeck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resources["rightDeck"] is Deck rightDeck)
+            {
+                rightDeck.Sort();
+            }
+        }
+
+        private void leftDeckListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MoveCard(true);
+        }
+
+        private void leftDeckListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MoveCard(true);
+            }
+        }
+
+    private void rightDeckListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MoveCard(false);
+            }
+        }
+
+        private void rightDeckListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MoveCard(false);
+        }
+    }
+}
